97eef9c81a2a3a31c3416102dbd2258476fc5dc3
Juan Manuel Cruz <cruz.jm.stafe@gmail.com>
2019-03-06 02:43:17 -0300

    Update the component and the style of the welcome display

--- src/Components/Welcome.elm
+++ src/Components/Welcome.elm
@@ -1,12 +1,27 @@
 module Components.Welcome exposing (view)
 
 import Html exposing (Html, div, h2, text)
-import Html.Attributes exposing (class)
+import Html.Attributes exposing (classList)
+import Html.Events exposing (onMouseLeave, onMouseOver)
+import Messages exposing (Msg(..))
 import Model exposing (Model)
 
 
-view : Model -> Html msg
-view { welcomeMessage } =
-    div [ class "welcome" ]
-        [ h2 [ class "welcome__message" ] [ text welcomeMessage ]
+view : Model -> Html Msg
+view { overWelcome, welcomeMessage } =
+    div
+        [ classList
+            [ ( "welcome", True )
+            , ( "welcome--hover", overWelcome )
+            ]
+        ]
+        [ h2
+            [ classList
+                [ ( "welcome__message", True )
+                , ( "welcome__message--hover", overWelcome )
+                ]
+            , onMouseLeave (ChangeWelcome False)
+            , onMouseOver (ChangeWelcome True)
+            ]
+            [ text welcomeMessage ]
         ]
