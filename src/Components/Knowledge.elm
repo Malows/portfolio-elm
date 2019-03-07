fd677f636834716c4cff127c84f7ee2d25f27030
Juan Manuel Cruz <cruz.jm.stafe@gmail.com>
2019-03-07 04:15:39 -0300

    Avoid using a wrapper node

--- src/Components/Knowledge.elm
+++ src/Components/Knowledge.elm
@@ -4,15 +4,16 @@ import Helpers exposing (picture)
 import Html exposing (Html, div, img, p, text)
 import Html.Attributes exposing (class, src)
 import Html.Events exposing (onClick)
+import Messages exposing (Msg(..))
 import Model exposing (Model)
 import Types exposing (Knowledge)
-import Messages exposing (Msg(..))
 
 
 knowledge : Knowledge -> Html Msg
 knowledge data =
     div [ class "knowledges-item", onClick (ShowKnowledge data) ]
-        [ img [ class "knowledges-item__img"
+        [ img
+            [ class "knowledges-item__img"
             , class "img-responsive"
             , src data.url
             ]
@@ -23,12 +24,14 @@ knowledge data =
 
 picInDescription : String -> Html msg
 picInDescription url =
-    div [ class "knowledges__description__img" ] [ img [ class "img-responsive" , src url ] [] ]
+    div [ class "knowledges__description__img" ] [ img [ class "img-responsive", src url ] [] ]
+
 
 stringSplitter : String -> List (Html msg)
 stringSplitter txt =
     String.split "\n" txt
-        |> List.map (\x -> p [] [ text x ])
+        |> List.map (\x -> p [ class "knowledges__description__text" ] [ text x ])
+
 
 knowledgeDescription : Maybe Knowledge -> Html msg
 knowledgeDescription selected =
@@ -42,10 +45,7 @@ knowledgeDescription selected =
                     [ p [ class "knowledges__description__text" ] [ text empty ] ]
 
                 Just item ->
-                    [ picInDescription item.url
-                    , div [ class "knowledges__description__text" ]
-                        ( stringSplitter item.description )
-                    ]
+                    [ picInDescription item.url ] ++ stringSplitter item.description
     in
     div [ class "knowledges__description" ] content
 
@@ -54,7 +54,9 @@ knowledgeLayout : Model -> Html Msg
 knowledgeLayout model =
     div [ class "knowledges" ]
         [ knowledgeDescription model.selectedKnowledge
-        , div [ class "knowledges__items"
+        , div
+            [ class "knowledges__items"
             , class "p-md"
-            ] (List.map knowledge model.knowledges)
+            ]
+            (List.map knowledge model.knowledges)
         ]
