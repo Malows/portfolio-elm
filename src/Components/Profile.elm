652ad9938994ffa9c1a4d9ef9b0d4c5415d495fc
Juan Manuel Cruz <cruz.jm.stafe@gmail.com>
2019-03-07 04:17:45 -0300

    Wrapping the social icons in a stacked circle

--- src/Components/Profile.elm
+++ src/Components/Profile.elm
@@ -1,7 +1,7 @@
 module Components.Profile exposing (view)
 
 import Components.Knowledge exposing (knowledgeLayout)
-import Html exposing (Html, a, div, h4, i, img, li, p, section, text, ul)
+import Html exposing (Html, a, div, h4, i, img, li, p, section, span, text, ul)
 import Html.Attributes exposing (class, href, id, src)
 import Messages exposing (Msg)
 import Model exposing (Model)
@@ -9,12 +9,32 @@ import Model exposing (Model)
 
 icon : String -> Html msg
 icon brand =
-    i [ class "fab", class ("fa-" ++ brand), class "fa-lg" ] []
+    span
+        [ class "fa-stack"
+        , class "fa-lg"
+        ]
+        [ i
+            [ class "fas"
+            , class "fa-circle"
+            , class "fa-stack-2x"
+            ]
+            []
+        , i
+            [ class "fab"
+            , class ("fa-" ++ brand)
+            , class "fa-stack-1x"
+            , class "fa-inverse"
+            ]
+            []
+        ]
 
 
 socialLink : String -> String -> Html msg
 socialLink brand url =
-    li [] [ a [ href url ] [ icon brand ] ]
+    li
+        []
+        [ a [ href url ] [ icon brand ]
+        ]
 
 
 disambleLink : ( String, String ) -> Html msg
