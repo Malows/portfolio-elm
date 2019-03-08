e47b82fd4e8b1e0da64e454745d2ac998ada699f
Juan Manuel Cruz <cruz.jm.stafe@gmail.com>
2019-03-08 00:59:24 -0300

    Now you can deactivate the active knowledge

--- src/Messages.elm
+++ src/Messages.elm
@@ -12,11 +12,31 @@ type Msg
     | ChangeWelcome Bool
 
 
+knowledgesCompare : Maybe Knowledge -> Knowledge -> Bool
+knowledgesCompare maybe know =
+    let
+        default =
+            Maybe.withDefault (Knowledge "" "" "") maybe
+    in
+    if default.name == "" then
+        True
+
+    else
+        default.name /= know.name
+
+
 update : Msg -> Model -> Model
 update msg model =
     case msg of
         ShowKnowledge knowledge ->
-            { model | selectedKnowledge = Just knowledge }
+            { model
+                | selectedKnowledge =
+                    if knowledgesCompare model.selectedKnowledge knowledge then
+                        Just knowledge
+
+                    else
+                        Nothing
+            }
 
         ChangeWelcome value ->
             { model | overWelcome = value }
